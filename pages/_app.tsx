2aa9d58957a382d41a0e2ea7a44b23ed69f72355
e.mahmodi <e.mahmodi@narvanventures.com>
2022-08-09 15:22:40 +0430

    add prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import {
+  Burger,
+  Button,
+  createEmotionCache,
+  createStyles,
+  MantineProvider
+} from "@mantine/core";
+import { AppProps } from "next/app";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const cache = createEmotionCache({ key: "mantine" });
+
+const useStyles = createStyles((theme) => ({ button: { width: "" } }));
+
+export default function App(props: AppProps) {
+  const { classes } = useStyles();
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider emotionCache={cache} withGlobalStyles withNormalizeCSS>
+        <Button>TEST</Button>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
-
-export default MyApp
